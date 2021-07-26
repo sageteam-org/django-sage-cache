a98f43bc02805038ed9ba18d7a717ddf7ff8a480
Mehran Rahmanzadeh <mrhnz13@gmail.com>
2021-07-26 11:52:18 +0430

    fix (sage_cache--filters): Fix CacheFilterBackend
    - fix rest_framework template renderer methods
    - fix 1 depth filters -> title='test'
    - get form template from sage_cache

--- sage_cache/filters/backend.py
+++ sage_cache/filters/backend.py
@@ -11,25 +11,63 @@ class CacheFilterBackend(DjangoFilterBackend):
     filter foreign_key/attribute from cache
     """
 
+    @property
+    def template(self):
+        return 'sage_cache/form.html'
+
+    def get_filterset(self, request, queryset, view):
+        """get filterset"""
+        filterset_class = self.get_filterset_class(view, queryset)
+        if not filterset_class:
+            return None
+        kwargs = self.get_filterset_kwargs(request, None, view)  # set None as queryset because its list
+        return filterset_class(**kwargs)
+
+    def get_filterset_class(self, view, queryset=None):
+        """return model class"""
+        if not hasattr(view, 'model_class'):
+            raise KeyError('you should set model_class in view')
+
+        MetaBase = getattr(self.filterset_base, 'Meta', object)
+
+        class AutoFilterSet(self.filterset_base):
+            class Meta(MetaBase):
+                model = view.model_class
+                fields = self.get_filterset_fields(view)
+
+        return AutoFilterSet
+
     def get_filterset_fields(self, view):
+        """get filterset_fields from view"""
         return getattr(view, 'filterset_fields', None)
+
+    def get_filter_depth(self, filter_kwargs: dict, level=0):
+        """get depth of dictionary"""
+        if not isinstance(filter_kwargs, dict) or not filter_kwargs:
+            return level
+        return max(self.get_filter_depth(filter_kwargs[key], level + 1) for key in filter_kwargs)
 
     def filter_queryset(self, request, queryset, view):
         """filter queryset from cache"""
-
         filter_kwargs = {}
         for param in request.query_params:
             for field in self.get_filterset_fields(view):
                 if param.startswith(field):
                     sub_fields = field.split('__')
                     length = len(sub_fields)
-                    for i in range(length):
-                        try:
-                            filter_kwargs[sub_fields[i]] = {sub_fields[i + 1]: None}
-                        except IndexError:
-                            filter_kwargs[sub_fields[i - 1]][sub_fields[i]] = request.query_params[param]
+                    if length == 1:  # filter field has no foreign key
+                        filter_kwargs[sub_fields[0]] = request.query_params[param]
+                    else:  # filter field has foreign key
+                        for i in range(length):
+                            try:
+                                filter_kwargs[sub_fields[i]] = {sub_fields[i + 1]: None}
+                            except IndexError:
+                                filter_kwargs[sub_fields[i - 1]][sub_fields[i]] = request.query_params[param]
 
-        return filter_related_from_cache(queryset, **filter_kwargs)
+        filter_depth = self.get_filter_depth(filter_kwargs)  # get depth of dict
+        if filter_depth == 1:  # filter has no foreign key
+            return filter_from_cache(queryset, **filter_kwargs)
+        return filter_related_from_cache(queryset, **filter_kwargs)  # filter with foreign key
 
 
 class CacheSearchBackend(SearchFilter):
