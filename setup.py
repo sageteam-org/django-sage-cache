c9b423a15f10164f4ae30c3e6e68f5d3ea3434f4
Mehran Rahmanzadeh <mrhnz13@gmail.com>
2021-07-27 09:33:20 +0430

    fix (pypi): Fix pypi setup files

--- setup.py
+++ setup.py
@@ -2,6 +2,8 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-sage-cache',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
     packages=find_packages(exclude=['tests*']),
     include_package_data=True,
     version='0.1.0',
@@ -9,8 +11,8 @@ setup(
     description='queryset caching for Django',
     author='Sage Team',
     author_email='mail@sageteam.org',
-    url='https://github.com/sageteam-org/django-sage-streaming',
-    download_url='https://github.com/sageteam-org/django-sage-streaming/archive/refs/tags/0.1.0.tar.gz',
+    url='https://github.com/sageteam-org/django-sage-cache',
+    download_url='https://github.com/sageteam-org/django-sage-cache/archive/refs/tags/0.1.0.tar.gz',
     keywords=['django', 'python', 'cache', 'SQL', 'queryset'],
     install_requires=[
         'Django',
